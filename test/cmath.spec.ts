15bc609602b33ead8253e3d8feab7f9681200088
Michel Albert <michel@albert.lu>
2023-12-11 07:04:53 +0100

    Fix unit-tests

--- test/cmath.spec.ts
+++ test/cmath.spec.ts
@@ -8,9 +8,9 @@ type TColor = {
 };
 
 const colors: TColor[] = [
-  { hex: "#123456", rgb: [18, 52, 86], hsl: [210, 0.654, 0.204] },
+  { hex: "#123456", rgb: [18, 51, 86], hsl: [210, 65.000, 20.400] },
   { hex: "#000000", rgb: [0, 0, 0], hsl: [0, 0, 0] },
-  { hex: "#ffffff", rgb: [255, 255, 255], hsl: [0, 0, 1] },
+  { hex: "#ffffff", rgb: [255, 255, 255], hsl: [0, 0, 100] },
 ];
 
 [0, 50, 128, 130, 255].map(r => {
@@ -31,28 +31,28 @@ const colors: TColor[] = [
 //   expect(getComplement(result)).toEqual(expected);
 // })
 
-// test('getComplement', () => {
-//   const result = new Color(10, 101, 200);
-//   const expected = new Color(0, 0, 0);
-//   expect(getComplement(result)).toEqual(expected);
-// })
+test('getComplement', () => {
+  const result = new Color(10, 101, 200);
+  const expected = new Color(199, 108, 10);
+  expect(getComplement(result)).toEqual(expected);
+})
 
 function test_conversions(color: TColor) {
   test(`RGB to HSL: ${color.rgb} -> ${color.hsl}`, () => {
     const result = rgb2hsl(...color.rgb);
-    result.map((value, index) => expect(value.toFixed(3)).toEqual(color.hsl[index].toFixed(3)));
+    result.map((value, index) => expect(value/100).toBeCloseTo(color.hsl[index]/100, 1));
   });
   test(`HSL to RGB: ${color.hsl} -> ${color.rgb}`, () => {
     const result = hsl2rgb(...color.hsl);
-    result.map((value, index) => expect(value.toFixed(3)).toEqual(color.rgb[index].toFixed(3)));
+    result.map((value, index) => expect(value / 100).toBeCloseTo(color.rgb[index] / 100, 1));
   });
   test(`HSL to RGB to HSL: ${color.hsl} -> ${color.rgb} -> ${color.hsl}`, () => {
-    const result = rgb2hsl(...hsl2rgb(...color.hsl)).map(value => value.toFixed(3));
-    result.map((value, index) => expect(value).toEqual(color.hsl[index].toFixed(3)));
+    const result = rgb2hsl(...hsl2rgb(...color.hsl)).map(value => value);
+    result.map((value, index) => expect(value / 100).toBeCloseTo(color.hsl[index] / 100, 1));
   });
   test(`RGB to HSL to RGB: ${color.rgb} -> ${color.hsl} -> ${color.rgb}`, () => {
-    const result = hsl2rgb(...rgb2hsl(...color.rgb)).map(value=> value.toFixed(3));
-    result.map((value, index) => expect(value).toEqual(color.rgb[index].toFixed(3)));
+    const result = hsl2rgb(...rgb2hsl(...color.rgb)).map(value=> value);
+    result.map((value, index) => expect(value / 100).toBeCloseTo(color.rgb[index] / 100, 1));
   });
 }
 
